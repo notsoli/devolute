688c0513d380ceaa26b4a26db39297636e4dc0d8
notsoli <samrandaofficial@gmail.com>
2024-10-16 23:47:07 -0400

    Added first example

--- examples/custom_bind_groups/app.ts
+++ examples/custom_bind_groups/app.ts
@@ -1,9 +1,9 @@
-import * as Devolute from '../src/app.js'
+import * as Devolute from '../../src/app.js'
 
 window.onload = async () => {
     const c1 = document.querySelector("#c1") as HTMLCanvasElement
     c1.width = window.innerWidth / 2
-    c1.height = window.innerHeight / 2
+    c1.height = window.innerWidth / 2
 
     const c1_frag = `
         @group(1) @binding(0) var<uniform> res: vec2f;
@@ -11,13 +11,15 @@ window.onload = async () => {
         @fragment
         fn fragment_main(@builtin(position) pos: vec4f) -> @location(0) vec4f {
             let position = pos.xy / res;
-            return vec4(position.x, position.y, 0.5 + sin(frame / 25.) / 2, 1.);
+            let size_val = min(res.x / 1000., 1.);
+            let size = vec4(vec3(size_val), 1.);
+            return vec4(position.x, position.y, 0.5 + sin(frame / 25.) / 2, 1.) * size;
         }
     `
 
     const c2 = document.querySelector("#c2") as HTMLCanvasElement
     c2.width = window.innerWidth / 1.5
-    c2.height = window.innerHeight / 1.5
+    c2.height = window.innerWidth / 1.5
 
     const c2_frag = `
         @group(1) @binding(0) var<uniform> res: vec2f;
@@ -25,13 +27,15 @@ window.onload = async () => {
         @fragment
         fn fragment_main(@builtin(position) pos: vec4f) -> @location(0) vec4f {
             let position = pos.xy / res;
-            return vec4(0.5 - sin(frame / 25.) / 2, position.x, position.y, 1.);
+            let size_val = min(res.x / 1000., 1.);
+            let size = vec4(vec3(size_val), 1.);
+            return vec4(0.5 - sin(frame / 25.) / 2, position.x, position.y, 1.) * size;
          }
     `
 
     const c3 = document.querySelector("#c3") as HTMLCanvasElement
     c3.width = window.innerWidth
-    c3.height = window.innerHeight
+    c3.height = window.innerWidth
 
     const c3_frag = `
         @group(1) @binding(0) var<uniform> res: vec2f;
@@ -39,38 +43,40 @@ window.onload = async () => {
         @fragment
         fn fragment_main(@builtin(position) pos: vec4f) -> @location(0) vec4f {
             let position = pos.xy / res;
-            return vec4(position.x, 0.5 - sin(frame / 25.) / 2, position.y, 1.);
+            let size_val = min(res.x / 1000., 1.);
+            let size = vec4(vec3(size_val), 1.);
+            return vec4(position.x, 0.5 - sin(frame / 25.) / 2, position.y, 1.) * size;
          }
     `
 
     const data = await Devolute.init({
         uniforms: {
-            frame: { type: "i32", value: 0 },
+            frame: { type: "f32", value: 0 },
         }
     })
 
     const p1 = await Devolute.createPass({ canvas: c1, fragment: c1_frag,
-        uniforms: { res: { type: "i32", value: [ window.innerWidth/2, window.innerHeight/2 ] } } })
+        uniforms: { res: { type: "vec2f", value: [ window.innerHeight/2, window.innerHeight/2 ] } } })
     const p2 = await Devolute.createPass({ canvas: c2, fragment: c2_frag,
-        uniforms: { res: { type: "i32", value: [ window.innerWidth/1.5, window.innerHeight/1.5 ] } } })
+        uniforms: { res: { type: "vec2f", value: [ window.innerHeight/1.5, window.innerHeight/1.5 ] } } })
     const p3 = await Devolute.createPass({ canvas: c3, fragment: c3_frag,
-        uniforms: { res: { type: "i32", value: [ window.innerWidth, window.innerHeight ] } } })
+        uniforms: { res: { type: "vec2f", value: [ window.innerHeight, window.innerHeight ] } } })
 
     Devolute.run(() => {
         data.frame++
     })
 
     window.onresize = () => {
-        p1.res = [ window.innerWidth/2, window.innerHeight/2 ]
+        p1.res = [ window.innerWidth/2, window.innerWidth/2 ]
         c1.width = window.innerWidth/2
-        c1.height = window.innerHeight/2
+        c1.height = window.innerWidth/2
 
-        p2.res = [ window.innerWidth/1.5, window.innerHeight/1.5 ]
+        p2.res = [ window.innerWidth/1.5, window.innerWidth/1.5 ]
         c2.width = window.innerWidth/1.5
-        c2.height = window.innerHeight/1.5
+        c2.height = window.innerWidth/1.5
 
-        p3.res = [ window.innerWidth, window.innerHeight ]
+        p3.res = [ window.innerWidth, window.innerWidth ]
         c3.width = window.innerWidth
-        c3.height = window.innerHeight
+        c3.height = window.innerWidth
     }
 }
